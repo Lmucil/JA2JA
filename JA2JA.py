f6218d2e34bff5ee483d2eb4847212778d009de4
Ri <112993351+Lmucil@users.noreply.github.com>
2023-03-28 19:08:12 -0400

    Add files via upload

---
+++ JA2JA.py
@@ -0,0 +1,44 @@
+import requests, simpleaudio, tempfile, json
+import speech_recognition as sr
+
+#音声を聞き取る
+mic = sr.Recognizer()
+try:
+    with sr.Microphone() as source:
+        print(u"聞き取り中...")
+        voice = mic.listen(source)
+        text = mic.recognize_google(voice, language="ja-JP")
+except:
+    print(u"聞き取れませんでした。")
+
+#HTTP リクエスト
+host = "127.0.0.1" 
+port = 50021
+
+params = (
+    ("text", text),
+    ("speaker", 14) #キャラの選択
+)
+
+response1 = requests.post(
+    f"http://{host}:{port}/audio_query",
+    params=params
+)
+
+response2 = requests.post(
+    f"http://{host}:{port}/synthesis",
+    headers={"Content-Type": "application/json"},
+    params=params,
+    data=json.dumps(response1.json())
+)
+
+with tempfile.TemporaryDirectory() as tmp:
+    with open(f"{tmp}/audi.wav", "wb") as f:
+        f.write(response2.content)
+        wav_obj = simpleaudio.WaveObject.from_wave_file(f"{tmp}/audi.wav")
+        play_obj = wav_obj.play()
+        play_obj.wait_done()
+
+
+
+# VOICEVOXエンジンを開いておいてください。
