fae3b9d67c81bb2f50cdef931e7a82683bc03c4a
Ri <112993351+Lmucil@users.noreply.github.com>
2023-03-30 22:47:50 -0400

    Update JA2JA.py

--- JA2JA.py
+++ JA2JA.py
@@ -1,5 +1,6 @@
 import requests, simpleaudio, tempfile, json
 import speech_recognition as sr
+import pykakasi
 
 #音声を聞き取る
 mic = sr.Recognizer()
@@ -11,20 +12,32 @@ try:
 except:
     print(u"聞き取れませんでした。")
 
+#全部カタカナに変換する
+kks = pykakasi.kakasi()
+kks.setMode("J", "K")  # 漢字からカタカナ
+kks.setMode("H", "K")  # ひらがなからカタカナ
+kks.setMode("K","K")   #カタカナはそのまま
+result = kks.convert(text)
+hiragana = set([chr(i) for i in range(12353, 12436)])  
+output = ''.join([r['hira'] if r['orig'] in hiragana else r['kana'] for r in result])
+
 #HTTP リクエスト
 host = "127.0.0.1" 
 port = 50021
 
+#文の設定とキャラ変更と
 params = (
-    ("text", text),
-    ("speaker", キャラの数字) #キャラクターの変更
+    ("text", output),
+    ("speaker", 14) 
 )
 
+#POST
 response1 = requests.post(
     f"http://{host}:{port}/audio_query",
     params=params
 )
 
+#もいっかい
 response2 = requests.post(
     f"http://{host}:{port}/synthesis",
     headers={"Content-Type": "application/json"},
@@ -32,13 +45,23 @@ response2 = requests.post(
     data=json.dumps(response1.json())
 )
 
+#WAVのダウンロード
 with tempfile.TemporaryDirectory() as tmp:
     with open(f"{tmp}/audi.wav", "wb") as f:
         f.write(response2.content)
-        wav_obj = simpleaudio.WaveObject.from_wave_file(f"{tmp}/audi.wav")
-        play_obj = wav_obj.play()
-        play_obj.wait_done()
+        result = simpleaudio.WaveObject.from_wave_file(f"{tmp}/audi.wav")
+        play = result.play()
+        play.wait_done()
 
 
 
+# VOICEVOXエンジンを開いておいてください。
+# VOICEVOXエンジンを開いておいてください。
+# VOICEVOXエンジンを開いておいてください。
+# VOICEVOXエンジンを開いておいてください。
+# VOICEVOXエンジンを開いておいてください。
+# VOICEVOXエンジンを開いておいてください。
+# VOICEVOXエンジンを開いておいてください。
+# VOICEVOXエンジンを開いておいてください。
+# VOICEVOXエンジンを開いておいてください。
 # VOICEVOXエンジンを開いておいてください。
